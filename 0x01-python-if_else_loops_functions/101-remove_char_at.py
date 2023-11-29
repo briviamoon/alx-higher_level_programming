8c9946d9ce6e4671acbbb222a0ab4c9675de26c2
briviamoon <briviamoon@gmail.com>
2023-11-30 01:04:59 +0300

    101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def remove_char_at(input_str, n):
-    if n < 0 or n >= len(input_str):
-        return "Invalid index"
-
-    return input_str[:n] + input_str[n+1:]
+    if n >= 0 or n < len(input_str):
+        return input_str[:n] + input_str[n+1:]
