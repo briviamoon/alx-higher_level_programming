9ff94841af97ee4970bb5e6148471130820f3964
briviamoon <briviamoon@gmail.com>
2023-12-02 18:13:13 +0300

    0_add.py

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/usr
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first intager
+        b: second intager
+
+    Returns:
+        The return value, a + b
+    """
+    return (a + b)
+
