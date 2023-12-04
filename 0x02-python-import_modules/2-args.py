0fc53eb4b90aaa5e524c0551ccbef15ff58af75b
briviamoon <briviamoon@gmail.com>
2023-12-04 14:39:14 +0300

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-import sys
+from sys import argv
 
-argc = len(sys.argv) - 1
+if __name__ == "__main__":
+    argc = len(argv) - 1
 
-if argc == 0:
-    print("0 arguments.")
-elif argc == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(argc))
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
 
-for arg in range(argc):
-    print("{}: {}".format(arg + 1, sys.argv[arg + 1]))
+    for arg in range(argc):
+        print("{}: {}".format(arg + 1, argv[arg + 1]))
