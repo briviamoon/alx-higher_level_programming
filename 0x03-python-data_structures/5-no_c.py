5d914cde23b17a011666de2c34f9eda5aef52272
briviamoon <briviamoon@gmail.com>
2023-12-06 00:34:47 +0300

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    filtered_string = ''.join(char for char in my_string if char.lower() not in {'c'})
+    filtered_string = ''.join(char for char in my_string
+                              if char.lower() not in {'c'})
     return (filtered_string)
